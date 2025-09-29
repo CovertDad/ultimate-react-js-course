2f5bd6f46d3ae81cdbb2e33f58fa774f4f8e9884
Gabe Covert <gabe@undercoverdesigns.com>
2025-09-29 14:25:32 -0400

    feat: Initial commit

---
+++ quotes-app/src/App.jsx
@@ -0,0 +1,12 @@
+import QuoteApp from "./Components/QuoteApp"
+import "./Components/QuoteApp.css"
+
+const App = () => {
+  return (
+    <div>
+      <QuoteApp></QuoteApp>
+    </div>
+  )
+}
+
+export default App
